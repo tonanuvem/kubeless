dca34456ef97b58845de55a11ffabbc786e9c6c0
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-03 19:04:24 -0300

    Add files via upload

---
+++ slack.py
@@ -0,0 +1,37 @@
+import json
+import requests
+
+url = "https://hooks.slack.com/services/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"
+canal = "#fiap"
+
+def handler(event, context):
+    try:
+        print(event)
+        if not 'msg' in event['data']:
+            return 'Campo vazio : msg'
+        texto=event['data']['msg']
+        print("Texto = "+ texto)
+        return postMSG_criada_para_o_slack(texto)
+        
+    except Exception as e:
+        erro = "Erro na function: " + repr(e);
+        print(erro)
+        return erro
+    
+def postMSG_criada_para_o_slack(msg):
+    # format payload for slack
+    sdata = formatForSlack(msg)
+    r = requests.post(url, sdata, headers={'Content-Type': 'application/json'})
+    if r.status_code == 200:
+      return 'SUCCEDED: Sent slack webhook.  Msg = ' + msg
+    else:
+      return 'FAILED: Send slack webhook'
+
+def formatForSlack(msg):
+  payload = {
+    "channel":canal,
+    "username":'ALUNO_Serverless',
+    "text": msg,
+    "icon_emoji":':cyclone'
+  }
+  return json.dumps(payload)
